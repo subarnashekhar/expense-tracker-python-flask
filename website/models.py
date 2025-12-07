1225984d14fe0979c1c6d230547882018cb1c5aa
subarnashekhar <subarnashekhar@gmail.com>
2025-12-06 22:02:09 -0600

    first commit

---
+++ website/models.py
@@ -0,0 +1,28 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class IncomeExpenses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(30), default='income', nullable=False)
+    category = db.Column(db.String(30), nullable=False, default='rent')
+    date = db.Column(db.DateTime(timezone=True), nullable=False, default=func.now())
+    amount = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
+    incomeexpenses = db.relationship('IncomeExpenses')
+
---
+++ website/views.py
@@ -0,0 +1,87 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Flask
+from flask_login import login_required, current_user
+from .models import Note, IncomeExpenses
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+app = Flask(__name__)
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+        entries = IncomeExpenses.query.order_by(IncomeExpenses.date.desc()).all()
+        return render_template("home.html", user=current_user, entries = entries)
+
+
+@views.route('/add', methods=['GET', 'POST'])
+@login_required
+def add_expense():
+    if request.method == 'POST':
+        amount = request.form.get('amount')
+        type = request.form.get('type')
+        category = request.form.get('category')
+
+        if amount == '':
+            flash('Please enter a valid amount', category='error')
+        elif type == '':
+            flash('Please enter a valid type', category='error')
+        elif category == '':
+            flash('Please enter a valid category', category='error')
+        else:
+            entry = IncomeExpenses(amount=amount, type=type, category=category)
+            db.session.add(entry)
+            db.session.commit()
+            flash('Income expenses added successfully')
+            return redirect(url_for('views.home'))
+
+    return render_template("add.html", user=current_user)
+
+@views.route('/delete-post/<int:entry_id>')
+@login_required
+def delete(entry_id):
+    entry = IncomeExpenses.query.get_or_404(int(entry_id))
+    db.session.delete(entry)
+    db.session.commit()
+    flash("Entry deleted", "success")
+    return redirect(url_for('views.home'), user=current_user)
+
+
+
+@views.route('/dashboard')
+@login_required
+def dashboard():
+    income_vs_expense = db.session.query(db.func.sum(IncomeExpenses.amount), IncomeExpenses.type).group_by(IncomeExpenses.type).order_by(IncomeExpenses.type).all()
+
+    category_comparison = db.session.query(db.func.sum(IncomeExpenses.amount), IncomeExpenses.category).group_by(IncomeExpenses.category).order_by(IncomeExpenses.category).all()
+
+    dates = db.session.query(db.func.sum(IncomeExpenses.amount), IncomeExpenses.date).group_by(IncomeExpenses.date).order_by(IncomeExpenses.date).all()
+
+    app.logger.info(income_vs_expense)
+    app.logger.info(category_comparison)
+    app.logger.info(dates)
+
+    income_category = []
+    for amounts, _ in category_comparison:
+        income_category.append(amounts)
+
+    income_expense = []
+    for total_amount, _ in income_vs_expense:
+        income_expense.append(total_amount)
+
+    over_time_expenditure = []
+    dates_label = []
+    for amount, date in dates:
+        dates_label.append(date.strftime("%m-%d-%y"))
+        over_time_expenditure.append(amount)
+
+    return render_template('dashboard.html',
+                            income_vs_expense=json.dumps(income_expense),
+                            income_category=json.dumps(income_category),
+                            over_time_expenditure=json.dumps(over_time_expenditure),
+                            dates_label =json.dumps(dates_label),
+                            user = current_user
+                        )
+
